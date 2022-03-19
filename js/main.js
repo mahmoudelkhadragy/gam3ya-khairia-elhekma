0cb47c69685764a95a0c1d6fe28ed5a57542426f
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-03-20 01:55:56 +0200

    add all project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.bar_icon').on('click', function () {
+    $('.side_bar').animate({ right: '0' }, 400);
+  })
+  $('.close_icon').on('click', function () {
+    $('.side_bar').animate({ right: '-210px' }, 400);
+  })
+
+  // carousel
+  $('.main-carousel').flickity({
+    // options
+    adaptiveHeight: true,
+    freeScroll: true,
+    wrapAround: true,
+    pageDots: false,
+    autoPlay: true
+  });
+})
