c76a7f1f0348b4a448c86713fc1d1d61824948a2
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2022-03-20 11:18:40 +0200

    add aos

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-  $('.bar_icon').on('click', function () {
-    $('.side_bar').animate({ right: '0' }, 400);
-  })
-  $('.close_icon').on('click', function () {
-    $('.side_bar').animate({ right: '-210px' }, 400);
-  })
+  AOS.init();
+  $(".bar_icon").on("click", function () {
+    $(".side_bar").animate({ right: "0" }, 400);
+  });
+  $(".close_icon").on("click", function () {
+    $(".side_bar").animate({ right: "-210px" }, 400);
+  });
 
   // carousel
-  $('.main-carousel').flickity({
+  $(".main-carousel").flickity({
     // options
     adaptiveHeight: true,
     freeScroll: true,
     wrapAround: true,
     pageDots: false,
-    autoPlay: true
+    autoPlay: true,
   });
-})
+});
